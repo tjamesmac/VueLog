3ba65483ee30721e73cab12cee07b42ff324c2fc
tjamesmac <tjamesmac@gmail.com>
2019-09-29 13:20:04 +0100

    Add static path

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 // const PORT = process.env.PORT;
 const PORT = 3000;
+console.log(path.join(__dirname, '../../dist'));
 
-app.get('/', (req, res) => {
-  res.send('hello');
-} );
+app.use('/', express.static(path.join(__dirname, '../../dist')));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
