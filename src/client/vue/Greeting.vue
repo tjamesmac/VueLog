c8ebe106c52b7de49a6a19bbb118af3c2ad5b201
tjamesmac <tjamesmac@gmail.com>
2019-09-29 14:12:32 +0100

    Add greeting component

---
+++ src/client/vue/Greeting.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ greeting }} {{noun}}!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Greeting',
+  data() {
+    return {
+      greeting: 'Hello',
+      noun: 'tom',
+    };
+  },
+};
+
+</script>
+<style lang='scss'>
+h1 {
+  color: red;
+}
+</style>
