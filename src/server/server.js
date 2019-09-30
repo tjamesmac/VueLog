0aff2701561f4455aa361a4ef051a45389c7589f
tjamesmac <tjamesmac@gmail.com>
2019-09-30 14:36:52 +0100

    Add db connection

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
 const app = express();
-// const PORT = process.env.PORT;
-const PORT = 3000;
-console.log(path.join(__dirname, '../../dist'));
+const PORT = process.env.PORT;
+
 
 app.use('/', express.static(path.join(__dirname, '../../dist')));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+} );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
